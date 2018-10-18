48bcdf191eeabb8c6cc20bcf7df7203a382760e5
mtsrp <matheus.rocha.pimenta@hotmail.com>
2018-10-17 21:22:54 -0300

    fix gitignore

--- Gerenc_fazenda/app/src/main/java/com/software/engenharia/gerenc_fazenda/Cadastrar.java
+++ Gerenc_fazenda/app/src/main/java/com/software/engenharia/gerenc_fazenda/Cadastrar.java
@@ -9,5 +9,6 @@ public class Cadastrar extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cadastrar);
+
     }
 }
