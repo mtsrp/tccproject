f5ce3ff7796ed2a825853feda0af273cac31230e
Vinicius Gustavo <vinicius@webjump.com.br>
2018-11-13 18:16:17 -0200

    logar.java fix

--- Gerenc_fazenda/app/src/main/java/com/software/engenharia/gerenc_fazenda/logar.java
+++ Gerenc_fazenda/app/src/main/java/com/software/engenharia/gerenc_fazenda/logar.java
@@ -29,7 +29,7 @@ public class logar extends AppCompatActivity {
         entra.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+
                 }
         });
 
