415475c9af64c98d1c2f5399d0a317fdeb2821e7
mtsrp <matheus.rocha.pimenta@hotmail.com>
2018-11-18 15:31:12 -0200

    Fix User CRUD

--- Gerenc_fazenda/app/src/main/java/com/software/engenharia/gerenc_fazenda/Funcionario/CentralFuncionario.java
+++ Gerenc_fazenda/app/src/main/java/com/software/engenharia/gerenc_fazenda/Funcionario/CentralFuncionario.java
@@ -1,29 +1,52 @@
 package com.software.engenharia.gerenc_fazenda.Funcionario;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.software.engenharia.gerenc_fazenda.AlteraFunc;
+import com.software.engenharia.gerenc_fazenda.PrincipalActivity;
 import com.software.engenharia.gerenc_fazenda.R;
 
 public class CentralFuncionario extends AppCompatActivity {
 
-    Button CadasFunc, AlterFunc, BuscaFunc, DemiteFunc;
+    Button AlterFunc, DemiteFunc, Sair;
+    TextView situa, permi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_central_funcionario);
 
-        BuscaFunc = findViewById(R.id.btn_RealizaBusca);
-        CadasFunc = findViewById(R.id.btn_cadastrafunc);
+        Sair = findViewById(R.id.btn_Sair);
         AlterFunc = findViewById(R.id.btn_Alterarfunc);
         DemiteFunc = findViewById(R.id.btn_DemitirFunc);
 
 
+        permi = findViewById(R.id.txt_permis);
+        situa = findViewById(R.id.txt_situacao);
+
+        Sair.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent principal = new Intent(CentralFuncionario.this, PrincipalActivity.class);
+                startActivity(principal);
+            }
+        });
+        DemiteFunc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                demis();
+            }
+        });
         AlterFunc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -31,5 +54,54 @@ public class CentralFuncionario extends AppCompatActivity {
                 startActivity(alter);
             }
         });
+    }
+
+    private void demis(){
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+
+        final EditText edittext = new EditText(CentralFuncionario.this);
+        alert.setMessage("Insira a data de demissão do funcionário:");
+        alert.setTitle("Demissão");
+
+        alert.setView(edittext);
+
+        alert.setPositiveButton("Confirma", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+
+                situa.setText("Situação: Desligado da Empresa");
+                permi.setText("Permissões: Sem permissões");
+                alter();
+
+            }
+        });
+
+        alert.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                // what ever you want to do with No option.
+            }
+        });
+
+        alert.show();
+    }
+
+    private void alter(){
+
+        //Cria o gerador do AlertDialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        //define o titulo
+        builder.setTitle("Demissão");
+        //define a mensagem
+        builder.setMessage("Funcionário desativado");
+
+        builder.setNeutralButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        });
+
+        //cria o AlertDialog
+        AlertDialog alterok = builder.create();
+        //Exibe
+        alterok.show();
     }
 }
--- Gerenc_fazenda/app/src/main/java/com/software/engenharia/gerenc_fazenda/PrincipalActivity.java
+++ Gerenc_fazenda/app/src/main/java/com/software/engenharia/gerenc_fazenda/PrincipalActivity.java
@@ -1,16 +1,20 @@
 package com.software.engenharia.gerenc_fazenda;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.software.engenharia.gerenc_fazenda.Funcionario.Busca_Funcionario;
 
 public class PrincipalActivity extends AppCompatActivity {
 
-    Button Logar, Cadastrar, Teste;
+    private AlertDialog alterok;
+    Button CadasSetor, Cadastrar, Teste;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,9 +22,16 @@ public class PrincipalActivity extends AppCompatActivity {
         setContentView(R.layout.principal);
 
         //Instanciamento dos botoes e text do layout
-        Logar = (Button) findViewById(R.id.btn_Login);
         Cadastrar = (Button) findViewById(R.id.btn_TesteCadastro);
         Teste =  findViewById(R.id.btn_teste);
+        CadasSetor = findViewById(R.id.btn_cadastrasetor);
+
+        CadasSetor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cadsetor();
+            }
+        });
 
         Cadastrar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,13 +39,6 @@ public class PrincipalActivity extends AppCompatActivity {
                 Intent cadastrar = new Intent(PrincipalActivity.this, Cadastrar_Funcionario.class);
                 startActivity(cadastrar);
 
-            }
-        });
-        Logar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent log =  new Intent(PrincipalActivity.this, logar.class);
-                startActivity(log);
             }
         });
 
@@ -45,5 +49,50 @@ public class PrincipalActivity extends AppCompatActivity {
                 startActivity(test);
             }
         });
+    }
+
+    private void cadsetor(){
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+
+        final EditText edittext = new EditText(PrincipalActivity.this);
+        alert.setMessage("Insira o nome do novo setor: ");
+        alert.setTitle("Cadastro de Setor");
+
+        alert.setView(edittext);
+
+        alert.setPositiveButton("Confirma", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+
+                alter();
+
+            }
+        });
+
+        alert.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                // what ever you want to do with No option.
+            }
+        });
+
+        alert.show();
+    }
+    private void alter(){
+        //Cria o gerador do AlertDialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        //define o titulo
+        builder.setTitle("Cadastrado");
+        //define a mensagem
+        builder.setMessage("Setor cadastrado com sucesso");
+
+        builder.setNeutralButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        });
+
+        //cria o AlertDialog
+        alterok = builder.create();
+        //Exibe
+        alterok.show();
     }
 }
